01c5aa06b20b13c4b311b179889940e5621b94f1
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-07-25 18:22:27 -0400

    Build helper function. Refactor code. Add comments

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@
 ///////////////
 
 let secretWord;
-
+let remainingGuesses = 8;
 
 
 ///////////////
@@ -16,10 +16,17 @@ function generateSecretWord() {
 	return fetch(wordFile)
 	.then((response) => response.text())
 	.then((text) => {
-		const wordArr = text.split('\n');
+		const wordArr = text.split('\r\n');
 		const randNum = Math.trunc(Math.random() * wordArr.length)
 		secretWord = wordArr[randNum]
+		console.log(secretWord); // temporary
 	})
+}
+
+function removeAllChildNodes(parent) {
+	while (parent.firstElementChild) {
+		parent.removeChild(parent.firstElementChild);
+	}
 }
 
 
@@ -29,19 +36,50 @@ function generateSecretWord() {
 //////////////////////
 
 const startGameBtn = document.getElementById('start-game-btn');
+const section2Ele = document.getElementById('section-2');
 
 window.addEventListener('load', () => {
-	generateSecretWord();
+	generateSecretWord(); // for now; difficulty would need to be selected first
 })
 
 startGameBtn.addEventListener('click', () => {
-	generateSecretWord();
+	const ul = document.getElementById('secret-word-container');
+	removeAllChildNodes(ul);
 	
 	for (let char of secretWord) {
-		const ul = document.getElementById('secret-word');
 		const li = document.createElement('li');
 		li.textContent = char.toUpperCase();
+		li.classList.add('guess');
 		ul.appendChild(li);
 	}
 
+	section2Ele.classList.toggle('hidden');
+	startGameBtn.classList.toggle('hidden');
+	document.querySelector('footer').classList.toggle('hidden');s
 })
+
+
+
+/* To Do:
+- Begin implementing the HTML canvas drawing of the Hangman
+
+- Start building out the logic for the event listeners that will be attached to each 
+of the letter containers. Make sure event delegation is working as intended
+
+- The startGameBtn listener will need to be updated so that it creates the secret word,
+but the textContent of each new li element doesnt show the char. The char should
+only be revealed after a correct guess
+
+- For the difficulty modes, that should be handled by a Modal window. Refer to previous
+projects and notes for building out the Modal
+*/
+
+/* Feature Ideas:
+- Difficulty Modes:
+	The harder the difficulty, the longer the words become
+	Note: 'electroencephalographically' is longest word I found (27 chars long)
+
+- Link to secret words Dictionary.com definition:
+	On win or lose, display a hyperlink to the words definition
+	Note: https://www.dictionary.com/browse/${word}
+*/
