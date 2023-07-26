c83161cf747c2c27831609587899f43f9b9611a8
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-07-26 12:57:48 -0400

    Implement additional game logic. Refactor code

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,7 @@
 
 let secretWord;
 let remainingGuesses = 8;
+const guessedLetters = [];
 
 
 ///////////////
@@ -18,7 +19,7 @@ function generateSecretWord() {
 	.then((text) => {
 		const wordArr = text.split('\r\n');
 		const randNum = Math.trunc(Math.random() * wordArr.length)
-		secretWord = wordArr[randNum]
+		secretWord = wordArr[randNum].toUpperCase()
 		console.log(secretWord); // temporary
 	})
 }
@@ -29,14 +30,40 @@ function removeAllChildNodes(parent) {
 	}
 }
 
+function checkWinOrLose() {
+	const allGuesses = document.querySelectorAll('.guess');
+	let guessedWord = '';
+	allGuesses.forEach(guessNode => guessedWord += guessNode.textContent)
+	
+	if (secretWord === guessedWord) {
+		// Handle win
+		console.log('you win :)');
+		document.querySelector('body').style.backgroundColor = 'rgb(105, 219, 58, 0.1)';
+		// Make the secret word letters and bottom border have a white glow
+		// Add a link to the words dictionary definition (https://www.dictionary.com/browse/${word})
+	} else if (remainingGuesses === 0) {
+		// Handle lose
+		console.log('you lose :(')
+		document.querySelector('body').style.backgroundColor = 'rgb(243, 115, 138, 0.1)';
+		// Add a link to the words dictionary definition (https://www.dictionary.com/browse/${word})
+	}
+}
+
+function reset() {
+	// Reset
+}
+
 
 
 //////////////////////
 // DOM MANIPULATION //
 //////////////////////
 
+let temp; // temporary
 const startGameBtn = document.getElementById('start-game-btn');
+const newGameBtn = document.getElementById('new-game-btn');
 const section2Ele = document.getElementById('section-2');
+const letterContainers = document.querySelectorAll('.letter-container');
 
 window.addEventListener('load', () => {
 	generateSecretWord(); // for now; difficulty would need to be selected first
@@ -45,18 +72,156 @@ window.addEventListener('load', () => {
 startGameBtn.addEventListener('click', () => {
 	const ul = document.getElementById('secret-word-container');
 	removeAllChildNodes(ul);
+
+	// Initial hangman drawing
 	
-	for (let char of secretWord) {
+	for (let idx in secretWord) {
 		const li = document.createElement('li');
-		li.textContent = char.toUpperCase();
-		li.classList.add('guess');
+		li.textContent = '';
+		li.classList.add('guess', `guess-${idx}`);
 		ul.appendChild(li);
 	}
 
 	section2Ele.classList.toggle('hidden');
 	startGameBtn.classList.toggle('hidden');
-	document.querySelector('footer').classList.toggle('hidden');s
+	newGameBtn.classList.toggle('hidden');
+	document.querySelector('footer').classList.toggle('hidden');
 })
+
+letterContainers.forEach( container => container.addEventListener('click', function(e) {
+	if (e.target.localName === 'li') {
+		const guessedLetter = e.target.textContent;
+		if (secretWord.includes(guessedLetter) && !guessedLetters.includes(guessedLetter)) {
+			for (idx in secretWord) {
+				if (secretWord[idx] === guessedLetter) document.querySelector(`.guess-${idx}`).textContent = guessedLetter;
+			}
+			e.target.classList.add('correct');
+			guessedLetters.push(guessedLetter);
+			checkWinOrLose();
+		} else if (!secretWord.includes(guessedLetter) && !guessedLetters.includes(guessedLetter)) {
+			remainingGuesses--;
+			e.target.classList.add('incorrect');
+			guessedLetters.push(guessedLetter);
+			checkWinOrLose();
+		}
+		console.log(remainingGuesses, guessedLetters); // temporary
+	}
+}))
+
+
+
+/////////////////////
+// HANGMAN DRAWING //
+/////////////////////
+
+
+// const canvas = document.getElementById('hangman');
+// const context = canvas.getContext("2d");
+
+// clearCanvas = () => {
+//   context.clearRect(0, 0, canvas.width, canvas.height)
+// }
+
+// Draw = (part) => {
+//    switch (part) {
+//       case 'gallows' :
+//         context.strokeStyle = '#444';
+//         context.lineWidth = 10; 
+//         context.beginPath();
+//         context.moveTo(175, 225);
+//         context.lineTo(5, 225);
+//         context.moveTo(40, 225);
+//         context.lineTo(25, 5);
+//         context.lineTo(100, 5);
+//         context.lineTo(100, 25);
+//         context.stroke();
+//         break;
+
+//       case 'head':
+//         context.lineWidth = 5;
+//         context.beginPath();
+//         context.arc(100, 50, 25, 0, Math.PI*2, true);
+//         context.closePath();
+//         context.stroke();
+//         break;
+      
+//       case 'body':
+//         context.beginPath();
+//         context.moveTo(100, 75);
+//         context.lineTo(100, 140);
+//         context.stroke();
+//         break;
+
+//       case 'rightHarm':
+//         context.beginPath();
+//         context.moveTo(100, 85);
+//         context.lineTo(60, 100);
+//         context.stroke();
+//         break;
+
+//       case 'leftHarm':
+//         context.beginPath();
+//         context.moveTo(100, 85);
+//         context.lineTo(140, 100);
+//         context.stroke();
+//         break;
+
+//       case 'rightLeg':
+//         context.beginPath();
+//         context.moveTo(100, 140);
+//         context.lineTo(80, 190);
+//         context.stroke();
+//         break;
+
+//       case 'rightFoot':
+//          context.beginPath();
+//          context.moveTo(82, 190);
+//          context.lineTo(70, 185);
+//          context.stroke();
+//       break;
+
+//       case 'leftLeg':
+//         context.beginPath();
+//         context.moveTo(100, 140);
+//         context.lineTo(125, 190);
+//         context.stroke();
+//       break;
+
+//       case 'leftFoot':
+//          context.beginPath();
+//          context.moveTo(122, 190);
+//          context.lineTo(135, 185);
+//          context.stroke();
+//       break;
+//    } 
+// }
+
+// const draws = [
+//    'gallows', 
+//    'head', 
+//    'body', 
+//    'rightHarm', 
+//    'leftHarm',
+//    'rightLeg',
+//    'leftLeg',
+//    'rightFoot',
+//    'leftFoot',
+// ]
+// var step = 0;
+
+
+// const next = document.getElementById('next')
+
+// next.addEventListener('click', function() {
+//   Draw(draws[step++])
+//   if (undefined === draws[step]) this.disabled = true;
+// });
+
+// document.getElementById('reset').addEventListener('click', function() {
+//   clearCanvas()
+//   step = 0
+//   next.disabled = false
+// })
 
 
 
